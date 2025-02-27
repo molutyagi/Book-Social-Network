77b7bf8f8386f65e7114ae14bfe147a379345399
imrya <aryanmolu2301@gmail.com>
2025-02-27 21:44:03 +0530

    init the project, docerized it, configured db and profile based properties

---
+++ book-social-network/src/test/java/com/booknetwork/api/BookSocialNetworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.booknetwork.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookSocialNetworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
